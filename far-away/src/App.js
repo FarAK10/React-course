83f765b4a18a50626aded9925b9d835b1d284343
Farkhod Botirov <botirovfarhod115@gmail.com>
2023-06-22 15:37:12 +0500

    feat: add on delete functionality

--- far-away/src/App.js
+++ far-away/src/App.js
@@ -2,7 +2,7 @@ import { useState } from "react";
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: false },
-  { id: 2, description: "Charger", quantity: 4, packed: true },
+  { id: 3, description: "Charger", quantity: 4, packed: true },
 ];
 
 function Logo() {
@@ -43,24 +43,40 @@ function Forms({ onAddItmes }) {
   );
 }
 
-function PackageList({ items }) {
+function PackageList({ items, onDeleteItem, onToggleItem }) {
   return (
     <div className="list">
       <ul>
         {items.map((item) => (
-          <Item item={item}></Item>
+          <Item
+            item={item}
+            onDeleteItem={onDeleteItem}
+            onToggleItem={onToggleItem}
+          ></Item>
         ))}
       </ul>
     </div>
   );
 }
-function Item({ item }) {
+function Item({ item, onDeleteItem, onToggleItem }) {
   return (
     <li>
+      <input
+        type="checkbox"
+        value={item.packed}
+        onChange={() => {
+          onToggleItem(item.id);
+        }}
+      ></input>
       <span style={item.packed ? { textDecoration: "line-through" } : {}}>
         {item.quantity} {item.description}
       </span>
-      <button style={{ color: "red", fontSize: "34px" }}>&times;</button>
+      <button
+        style={{ color: "red", fontSize: "34px" }}
+        onClick={() => onDeleteItem(item.id)}
+      >
+        &times;
+      </button>
     </li>
   );
 }
@@ -74,15 +90,34 @@ function Stats() {
 }
 
 export default function App() {
-  const [items, setItems] = useState([]);
+  const [items, setItems] = useState(initialItems);
+
   function handleAddItem(item) {
     setItems((items) => [...items, item]);
+  }
+
+  function handleToggleItem(id) {
+    setItems((items) =>
+      items.map((item) =>
+        item.id === id ? { ...item, packed: !item.packed } : item
+      )
+    );
+  }
+
+  function handleDeleteItem(id) {
+    setItems((items) => {
+      return [...items.filter((el) => el.id !== id)];
+    });
   }
   return (
     <div className="app">
       <Logo></Logo>
       <Forms onAddItmes={handleAddItem}></Forms>
-      <PackageList items={items}></PackageList>
+      <PackageList
+        items={items}
+        onDeleteItem={handleDeleteItem}
+        onToggleItem={handleToggleItem}
+      ></PackageList>
       <Stats></Stats>
     </div>
   );
