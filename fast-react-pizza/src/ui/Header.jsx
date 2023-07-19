3126f1886196d8a90c4c9e12096dc0d37e6dd232
london jack <botirovfarhod115@gmail.com>
2023-07-19 16:15:23 +0500

    feat: add tailwind

---
+++ fast-react-pizza/src/ui/Header.jsx
@@ -0,0 +1,16 @@
+import { Link } from 'react-router-dom';
+import SearchOrder from '../features/order/SearchOrder';
+import UserName from '../features/user/UserName';
+
+export default function Header() {
+  return (
+    <header className="bg-yellow-500 uppercase">
+      <Link to="/" className="tracking-[5px]">
+        Fast React Pizza Co.
+      </Link>
+
+      <SearchOrder></SearchOrder>
+      <UserName></UserName>
+    </header>
+  );
+}
