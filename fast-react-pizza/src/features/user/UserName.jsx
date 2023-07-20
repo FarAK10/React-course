ea9ae46d00d62f7885d8a386fab024375a18d213
london jack <botirovfarhod115@gmail.com>
2023-07-20 10:19:16 +0500

    feat: add styles via tailwind

--- fast-react-pizza/src/features/user/UserName.jsx
+++ fast-react-pizza/src/features/user/UserName.jsx
@@ -1,3 +1,5 @@
-export default function UserName() {
-  return <div className="text-sm font-semibold">Jonas</div>;
+function Username() {
+  return <div className="hidden text-sm font-semibold md:block">Jonas</div>;
 }
+
+export default Username;
--- fast-react-pizza/src/ui/Home.jsx
+++ fast-react-pizza/src/ui/Home.jsx
@@ -2,8 +2,8 @@ import CreateUser from '../features/user/CreateUser';
 
 function Home() {
   return (
-    <div>
-      <h1 className="text-center text-xl font-semibold text-stone-700">
+    <div className="my-10 px-4 text-center sm:my-16">
+      <h1 className="mb-8  text-xl font-semibold md:text-3xl">
         The best pizza.
         <br />
         <span className="text-yellow-500">
@@ -11,7 +11,7 @@ function Home() {
         </span>
       </h1>
 
-      <CreateUser></CreateUser>
+      <CreateUser />
     </div>
   );
 }
